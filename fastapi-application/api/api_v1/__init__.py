1bedb195d7554dbbc4d407c26a9474c4b0928f48
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-06-07 10:44:09 +0300

    delete(Users): Показательные изменения для старта работы

--- fastapi-application/api/api_v1/__init__.py
+++ fastapi-application/api/api_v1/__init__.py
@@ -2,12 +2,8 @@ from fastapi import APIRouter
 
 from core.config import settings
 
-from .users import router as users_router # TODO DELETE
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
-router.include_router(
-    users_router,# TODO DELETE
-    prefix=settings.api.v1.users,# TODO DELETE
-)
+# router.include_router()
--- fastapi-application/core/models/__init__.py
+++ fastapi-application/core/models/__init__.py
@@ -1,9 +1,7 @@
 __all__ = (
     "db_helper",
     "Base",
-    "User", # TODO DELETE
 )
 
 from .base import Base
 from .db_helper import db_helper
-from .user import User  # TODO DELETE

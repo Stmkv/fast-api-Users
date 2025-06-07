44405ea51f2ac28825e5ce1164947ba54b8e2862
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-06-07 12:06:13 +0300

    feat(method): User database

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,10 +1,18 @@
+from typing import TYPE_CHECKING
 from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
 )
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):  # type: ignore[misc]
-    pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)

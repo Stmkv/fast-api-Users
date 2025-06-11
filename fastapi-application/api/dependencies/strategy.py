e5e4cff4d878d0b44c2d737ae0dd986b0da2fb45
Макаров Степан Александрович <st.mkv03@gmail.com>
2025-06-11 10:47:25 +0300

    feat(create get db strategy dependencies)

--- fastapi-application/api/dependencies/strategy.py
+++ fastapi-application/api/dependencies/strategy.py
@@ -9,7 +9,7 @@ from fastapi_users.authentication.strategy.db import (
 )
 
 from core.config import settings
-from core.models.access_token import get_access_tokens_db
+from.access_tokens import get_access_tokens_db
 
 
 if TYPE_CHECKING:
